a4ed04da5826a10524f2c987311538a19f41e0c3
Armand Winant <bastien.winant@mail.mcgill.ca>
2025-01-17 00:15:42 +0100

    run postgres container and update gitignore

--- 01-docker-terraform/2-docker-sql/docker-commands.sh
+++ 01-docker-terraform/2-docker-sql/docker-commands.sh
@@ -3,3 +3,12 @@ docker build -t <name>:<tag> .
 
 # run container based on image
 docker run -it --rm <name>:<tag>
+
+# run postgres with env variables
+docker run --rm -it \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v ny_taxi_postgres_data:/var/lib/postgresql/data \
+  -p 5433:5432 \
+postgres:latest
