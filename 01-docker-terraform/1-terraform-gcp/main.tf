dba6df269de9b0eabf095fb0c713d992b6128ef1
Armand Winant <bastien.winant@mail.mcgill.ca>
2025-01-19 03:36:53 +0100

    create a gcp bucket from terraform

---
+++ 01-docker-terraform/1-terraform-gcp/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "durable-cacao-448302-v7"
+  region  = "europe-west3"
+  credentials = "./keys/my-creds.json"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "durable-cacao-448302-v7-terraform-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
