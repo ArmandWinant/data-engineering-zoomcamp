379ce294c331f998496852497e74fbbb20ba4657
Armand Winant <bastien.winant@mail.mcgill.ca>
2025-03-18 17:18:15 +0100

    spark sql script

---
+++ 05-batch-processing/spark_sql.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from pyspark.sql import SparkSession, functions as F
+
+spark = SparkSession\
+  .builder\
+  .master("spark://Bastiens-MacBook-Pro.local:7077")\
+  .appName('test')\
+  .getOrCreate()
+
+
+# In[20]:
+
+
+df_green = spark.read.parquet('./data/pq/green/*/*')
+df_green = df_green\
+  .withColumnRenamed('lpep_pickup_datetime', 'pickup_datetime')\
+  .withColumnRenamed('lpep_dropoff_datetime', 'dropoff_datetime')
+
+df_yellow = spark.read.parquet('./data/pq/yellow/*/*')
+df_yellow = df_yellow\
+  .withColumnRenamed('tpep_pickup_datetime', 'pickup_datetime')\
+  .withColumnRenamed('tpep_dropoff_datetime', 'dropoff_datetime')
+
+common_columns = [col for col in df_green.columns if col in df_yellow.columns]
+
+df_green_sel = df_green.select(common_columns).withColumn('service_type', F.lit('green'))
+df_yellow_sel = df_yellow.select(common_columns).withColumn('service_type', F.lit('yellow'))
+
+df_trips_data = df_green_sel.unionAll(df_yellow_sel)
+df_trips_data.createOrReplaceTempView('trip_data')
+
+df_result = spark.sql("""
+  SELECT 
+    PULocationID AS revenue_zone,
+    date_trunc("month", "pickup_datetime") AS revenue_month,
+    service_type, 
+    SUM(fare_amount) AS revenue_monthly_fare,
+    SUM(extra) AS revenue_monthly_extra,
+    SUM(mta_tax) AS revenue_monthly_mta_tax,
+    SUM(tip_amount) AS revenue_monthly_tip_amount,
+    SUM(tolls_amount) AS revenue_monthly_tolls_amount,
+    SUM(improvement_surcharge) AS revenue_monthly_improvement_surcharge,
+    SUM(total_amount) AS revenue_monthly_total_amount,
+    AVG(passenger_count) AS avg_monthly_passenger_count,
+    AVG(trip_distance) AS avg_monthly_trip_distance
+  FROM trip_data
+  GROUP BY revenue_zone, revenue_month, service_type
+""")
+df_result.coalesce(1).write.parquet('data/report/revenue/', mode='overwrite')
+
+spark.stop()
+
